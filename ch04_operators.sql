9b327a95d14064dde008ba3a595ba00cfa9cff7e
ljyGmail <liangjy881@gmail.com>
2025-02-19 11:24:10 +0900

    21 第4章运算符课后练习

--- ch04_operators.sql
+++ ch04_operators.sql
@@ -205,3 +205,77 @@ from dual;
 # 在一定范围内满足: 每向左移动1位，相当于乘以2，每向右移动一位，相当于除以2
 select 4 << 1, 8 >> 1
 from dual;
+
+# 课后练习
+# 1.选择工资不在5000到12000的员工的姓名和工资
+select last_name, salary
+from employees
+where salary not between 5000 and 12000;
+
+# 2.选择在20或50号部门工作的员工姓名和部门号
+select last_name, department_id
+from employees
+where department_id in (20, 50);
+
+# 3.选择公司中没有管理者的员工姓名及job_id
+select last_name, job_id
+from employees
+where manager_id is null;
+
+select last_name, job_id
+from employees
+where manager_id <=> null;
+
+# 4.选择公司中有奖金的员工姓名，工资和奖金级别
+select last_name, salary, commission_pct
+from employees
+where commission_pct is not null;
+
+select last_name, salary, commission_pct
+from employees
+where not commission_pct <=> null;
+
+# 5.选择员工姓名的第三个字母是a的员工姓名
+select last_name
+from employees
+where last_name like '__a%';
+
+# 6.选择姓名中有字母a和k的员工姓名
+select last_name
+from employees
+where last_name like '%a%'
+  and last_name like '%k%';
+
+select last_name
+from employees
+where last_name like '%a%k%'
+   or last_name like '%k%a%';
+
+# 7.显示出表 employees 表中 first_name 以 'e'结尾的员工信息
+select first_name, last_name
+from employees
+where first_name like '%e';
+
+select first_name, last_name
+from employees
+where first_name regexp 'e$';
+# 以e开头的写法: '^e'
+
+# 8.显示出表 employees 部门编号在 80-100 之间的姓名、工种
+select last_name, job_id
+from employees
+# 方式1: 推荐
+# where department_id between 80 and 100;
+# 方式2: 推荐
+where department_id >= 80
+  and department_id <= 100;
+
+select last_name, job_id
+from employees
+# 方式3: 仅适用于本题的方式
+where department_id in (80, 90, 100);
+
+# 9.显示出表 employees 的 manager_id 是 100,101,110 的员工姓名、工资、管理者id
+select last_name, salary, manager_id
+from employees
+where manager_id in (100, 101, 110);
