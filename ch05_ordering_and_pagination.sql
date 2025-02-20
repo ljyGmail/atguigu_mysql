1b113eb2d434e8f82f2b070c6af659314476d665
ljyGmail <liangjy881@gmail.com>
2025-02-20 14:39:14 +0900

    24 第5章排序与分页课后练习

--- ch05_ordering_and_pagination.sql
+++ ch05_ordering_and_pagination.sql
@@ -100,3 +100,23 @@ limit 1;
 
 # 2.4 LIMIT 可以使用在MySQL、PGSQL、MariaDB、SQLite 等数据库中使用，表示分页。
 # 不能使用在SQL Server、DB2、Oracle。
+
+# 课后练习
+#1. 查询员工的姓名和部门号和年薪，按年薪降序,按姓名升序显示
+select last_name, department_id, salary * 12 annual_salary
+from employees
+order by annual_salary desc, last_name asc;
+
+#2. 选择工资不在 8000 到 17000 的员工的姓名和工资，按工资降序，显示第21到40位置的数据
+select last_name, salary
+from employees
+where salary not between 8000 and 17000
+order by salary desc
+limit 20, 20;
+
+#3. 查询邮箱中包含 e 的员工信息，并先按邮箱的字节数降序，再按部门号升序
+select employee_id, last_name, email, department_id
+from employees
+# where email like '%e%'
+where email regexp '[e]'
+order by length(email) desc, department_id;
