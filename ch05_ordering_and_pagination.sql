8f175381a2cf38e4b22dc6a5279e763c78b2ffd0
ljyGmail <liangjy881@gmail.com>
2025-02-20 11:23:35 +0900

    23 LIMIT实现分页操作

--- ch05_ordering_and_pagination.sql
+++ ch05_ordering_and_pagination.sql
@@ -48,3 +48,55 @@ order by department_id desc;
 select employee_id, salary, department_id
 from employees
 order by department_id desc, salary asc;
+
+# 2. 分页
+# 2.1 mysql使用limit实现数据的分页显示
+
+# 需求1: 每页显示20条记录，此时显示第1页
+select employee_id, last_name
+from employees
+limit 0, 20;
+
+# 需求2: 每页显示20条记录，此时显示第2页
+select employee_id, last_name
+from employees
+limit 20, 20;
+
+# 需求3: 每页显示20条记录，此时显示第3页
+select employee_id, last_name
+from employees
+limit 40, 20;
+
+# 需求: 每页显示pageSize条记录，此时显示第pageNo页:
+# 公式: LIMIT (pageNo - 1) * pageSize, pageSize;
+
+# 2.2 WHERE ... ORDER BY ... LIMIT 声明顺序如下:
+
+# LIMIT的格式: 严格来说，LIMIT 位置偏移量, 条目数
+# 结构"LIMIT 0, 条目数"等价于"LIMIT 条目数"
+select employee_id, last_name, salary
+from employees
+where salary > 6000
+order by salary desc
+# limit 0, 10;
+limit 10;
+
+# 练习: 表里有107条数据，只想要显示第32、33条数据怎么办？
+select employee_id, last_name
+from employees
+limit 31, 2;
+
+# 2.3 MySQL8.0新特性: LIMIT ... OFFSET ...
+select employee_id, last_name
+from employees
+limit 2 offset 31;
+
+# 练习: 查询员工表中工资最高的员工信息
+select employee_id, last_name, salary
+from employees
+order by salary desc
+# limit 0, 1;
+limit 1;
+
+# 2.4 LIMIT 可以使用在MySQL、PGSQL、MariaDB、SQLite 等数据库中使用，表示分页。
+# 不能使用在SQL Server、DB2、Oracle。
