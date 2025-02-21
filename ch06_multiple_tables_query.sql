a9ed7ac7d15a4180ea690ace9df9a4209f6786ed
ljyGmail <liangjy881@gmail.com>
2025-02-21 18:26:47 +0900

    31 第6章多表查询课后练习

--- ch06_multiple_tables_query.sql
+++ ch06_multiple_tables_query.sql
@@ -368,3 +368,164 @@ INSERT INTO t_emp(NAME, age, deptId, empno)
 VALUES ('张无忌', 25, 5, 100009);
 INSERT INTO t_emp(NAME, age, deptId, empno)
 VALUES ('韦小宝', 18, NULL, 100010);
+
+# 【题目】
+# 1.显示所有员工的姓名，部门号和部门名称。
+SELECT e.last_name, e.department_id, d.department_name
+FROM employees e
+         LEFT JOIN departments d
+                   ON e.department_id = d.department_id;
+
+# 2.查询90号部门员工的job_id和90号部门的location_id
+SELECT e.job_id, d.location_id
+FROM employees e
+         JOIN departments d
+              ON e.department_id = d.department_id
+WHERE e.department_id = 90;
+
+# 3.选择所有有奖金的员工的 last_name , department_name , location_id , city
+SELECT e.last_name, e.commission_pct, d.department_name, d.location_id, l.city
+FROM employees e
+         LEFT JOIN departments d
+                   ON e.department_id = d.department_id
+         LEFT JOIN locations l
+                   ON d.location_id = l.location_id
+WHERE e.commission_pct IS NOT NULL; # 也应该是35条记录
+
+SELECT *
+FROM employees
+WHERE commission_pct IS NOT NULL;
+# 35条记录
+
+# 4.选择city在Toronto工作的员工的 last_name , job_id , department_id , department_name
+SELECT e.last_name, e.job_id, e.department_id, d.department_name
+FROM employees e
+         JOIN departments d
+              ON e.department_id = d.department_id
+         JOIN locations l
+              ON d.location_id = l.location_id
+WHERE l.city = 'Toronto';
+
+# SQL92语法
+SELECT e.last_name, e.job_id, e.department_id, d.department_name
+FROM employees e,
+     departments d,
+     locations l
+WHERE e.department_id = d.department_id
+  AND d.location_id = l.location_id
+  AND l.city = 'Toronto';
+
+# 5.查询员工所在的部门名称、部门地址、姓名、工作、工资，其中员工所在部门的部门名称为’Executive’
+SELECT d.department_name, l.street_address, e.last_name, e.job_id, e.salary
+FROM departments d
+         LEFT JOIN employees e
+                   ON e.department_id = d.department_id
+         LEFT JOIN locations l ON d.location_id = l.location_id
+WHERE d.department_name = 'Executive';
+
+# 6.选择指定员工的姓名，员工号，以及他的管理者的姓名和员工号，结果类似于下面的格式
+# employees Emp# manager Mgr#
+# kochhar 101 king 100
+
+SELECT emp.last_name "employees", emp.employee_id "Emp#", mgr.last_name "manager", mgr.employee_id "Mgr#"
+FROM employees emp
+         LEFT JOIN employees mgr
+                   ON emp.manager_id = mgr.employee_id;
+
+# 7.查询哪些部门没有员工
+SELECT d.department_id, d.department_name
+FROM departments d
+         LEFT JOIN employees e
+                   ON d.department_id = e.department_id
+WHERE e.department_id IS NULL;
+
+# 本题也可以使用子查询: 暂时不讲
+SELECT department_id, department_name
+FROM departments d
+WHERE NOT EXISTS(SELECT * FROM employees e WHERE e.department_id = d.department_id);
+
+# 8. 查询哪个城市没有部门
+SELECT l.location_id, l.city
+FROM locations l
+         LEFT JOIN departments d
+                   ON l.location_id = d.location_id
+WHERE d.location_id IS NULL;
+
+# 举例验证上面的查询是否正确
+SELECT department_id
+FROM departments
+WHERE department_id IN (1000, 1100, 1200, 1300, 1600);
+
+# 9. 查询部门名为 Sales 或 IT 的员工信息
+SELECT e.employee_id, e.last_name, d.department_name
+FROM employees e
+         JOIN departments d
+              ON e.department_id = d.department_id
+WHERE d.department_name IN ('Sales', 'IT');
+
+# 【题目】
+DESC t_dept;
+DESC t_emp;
+#1.所有有门派的人员信息
+# （ A、B两表共有）
+SELECT e.empno, e.name, d.deptName
+FROM t_emp e
+         JOIN t_dept d
+              ON e.deptId = d.id;
+#2.列出所有用户，并显示其机构信息
+# （A的全集）
+SELECT e.empno, e.name, d.deptName
+FROM t_emp e
+         LEFT JOIN t_dept d
+                   ON e.deptId = d.id;
+
+#3.列出所有门派
+# （B的全集）
+SELECT *
+FROM t_dept;
+
+#4.所有不入门派的人员
+# （A的独有）
+SELECT *
+FROM t_emp e
+         LEFT JOIN t_dept d
+                   ON e.deptId = d.id
+WHERE d.id IS NULL;
+
+#5.所有没人入的门派
+# （B的独有）
+SELECT *
+FROM t_emp e
+         RIGHT JOIN t_dept d
+                    ON e.deptId = d.id
+WHERE e.deptId IS NULL;
+
+#6.列出所有人员和机构的对照关系
+# (AB全有)
+#MySQL Full Join的实现 因为MySQL不支持FULL JOIN,下面是替代方法
+#left join + union(可去除重复数据)+ right join
+
+SELECT e.empno, e.name, d.deptName
+FROM t_emp e
+         LEFT JOIN t_dept d
+                   ON e.deptId = d.id
+UNION ALL
+SELECT e.empno, e.name, d.deptName
+FROM t_emp e
+         RIGHT JOIN t_dept d
+                    ON e.deptId = d.id
+WHERE e.deptId IS NULL;
+
+#7.列出所有没入派的人员和没人入的门派
+# （A的独有+B的独有）
+SELECT *
+FROM t_emp e
+         LEFT JOIN t_dept d
+                   ON e.deptId = d.id
+WHERE d.id IS NULL
+UNION ALL
+SELECT *
+FROM t_emp e
+         RIGHT JOIN t_dept d
+                    ON e.deptId = d.id
+WHERE e.deptId IS NULL;
