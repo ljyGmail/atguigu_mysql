54458d100bf8c76698ffa841506b5885fed30a43
ljyGmail <liangjy881@gmail.com>
2025-02-22 21:33:14 +0900

    35 日期时间类型的函数讲解

--- ch07_single_row_functions.sql
+++ ch07_single_row_functions.sql
@@ -107,3 +107,105 @@ FROM dual;
 SELECT employee_id, NULLIF(LENGTH(first_name), LENGTH(last_name)) "compare"
 FROM employees;
 
+# 3. 日期和时间函数
+
+# 3.1 获取日期、时间
+SELECT CURDATE(), CURRENT_DATE(), CURTIME(), NOW(), SYSDATE(), UTC_DATE(), UTC_TIME()
+FROM dual;
+
+SELECT CURDATE(), CURDATE() + 0, CURTIME(), CURTIME() + 0, NOW() + 0
+FROM dual;
+
+# 3.2 日期与时间戳的转换
+SELECT UNIX_TIMESTAMP()
+     , UNIX_TIMESTAMP('2025-02-22 20:28:31')
+     , FROM_UNIXTIME(1740223607)
+     , FROM_UNIXTIME(1740223711)
+FROM dual;
+
+# 3.3 获取月份、星期、星期数、天数等函数
+SELECT YEAR(CURDATE()),
+       MONTH(CURDATE()),
+       DAY(CURDATE()),
+       HOUR(CURTIME()),
+       MINUTE(NOW()),
+       SECOND(SYSDATE())
+FROM dual;
+
+SELECT MONTHNAME('2021-10-26'),
+       DAYNAME('2021-10-26'),
+       WEEKDAY('2021-10-26'),
+       QUARTER(CURDATE()),
+       WEEK(CURDATE()),
+       DAYOFYEAR(NOW()),
+       DAYOFMONTH(NOW()),
+       DAYOFWEEK(NOW())
+FROM dual;
+
+# 3.4 日期的操作函数
+SELECT EXTRACT(SECOND FROM NOW()),
+       EXTRACT(DAY FROM NOW()),
+       EXTRACT(HOUR FROM NOW()),
+       EXTRACT(QUARTER FROM '2025-05-12')
+FROM dual;
+
+# 3.5 时间和秒钟转换的函数
+SELECT TIME_TO_SEC(CURTIME()), SEC_TO_TIME(74563)
+FROM dual;
+
+# 3.6 计算日期和时间的函数
+SELECT NOW()
+     , DATE_ADD(NOW(), INTERVAL 1 YEAR)
+     , DATE_ADD(NOW(), INTERVAL -1 YEAR)
+     , DATE_SUB(NOW(), INTERVAL 1 YEAR)
+FROM dual;
+
+SELECT DATE_ADD(NOW(), INTERVAL 1 DAY)                               AS col1,
+       DATE_ADD('2025-02-22 20:48:30', INTERVAL 1 SECOND)            AS col2,
+       ADDDATE('2025-02-22 20:48:40', INTERVAL 1 SECOND)             AS col3,
+       DATE_ADD('2025-02-22 20:48:40', INTERVAL '1_1' MINUTE_SECOND) AS col4,
+       DATE_ADD(NOW(), INTERVAL -1 YEAR)                             AS col5, # 可以是负数
+       DATE_ADD(NOW(), INTERVAL '1_1' YEAR_MONTH)                    AS col6  # 需要单引号
+FROM dual;
+
+SELECT ADDTIME(NOW(), 20),
+       SUBTIME(NOW(), '1:1:3'),
+       DATEDIFF(NOW(), '2025-02-01'),
+       TIMEDIFF(NOW(), '2025-02-21 08:02:10'),
+       FROM_DAYS(366),
+       TO_DAYS('0000-12-25'),
+       LAST_DAY(NOW()),
+       MAKEDATE(YEAR(NOW()), 12),
+       MAKETIME(10, 21, 23),
+       PERIOD_ADD(20200101010101, 10)
+FROM dual;
+
+# 3.7 日期的格式化与解析
+# 格式化: 日期 --> 字符串
+# 解析: 字符串 --> 日期
+
+# 此时我们谈的是日期的显式格式化和解析
+
+# 此前，我们接触过隐式的格式化或解析
+
+SELECT *
+FROM employees
+WHERE hire_date = '1993-01-13';
+
+# 格式化
+SELECT DATE_FORMAT(CURDATE(), '%Y-%M-%D'),
+       DATE_FORMAT(NOW(), '%Y-%m-%d'),
+       TIME_FORMAT(CURTIME(), '%h:%i:%S'),
+       DATE_FORMAT(NOW(), '%Y-%M-%D %h-%i:%S %W %w %T %r')
+FROM dual;
+
+# 解析: 格式化的逆过程
+SELECT STR_TO_DATE('2025-February-22nd 09-25:56 Saturday 6', '%Y-%M-%D %h-%i:%S %W %w')
+FROM dual;
+
+SELECT GET_FORMAT(DATE, 'USA')
+FROM dual;
+
+SELECT DATE_FORMAT(CURDATE(), GET_FORMAT(DATE, 'USA'))
+FROM dual;
+
