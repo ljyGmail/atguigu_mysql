0b80af4bba71bd237e03c4a45e43730e007047a4
ljyGmail <liangjy881@gmail.com>
2025-02-25 14:07:56 +0900

    42 第8章聚合函数课后练习

--- ch08_aggregate_functions.sql
+++ ch08_aggregate_functions.sql
@@ -187,13 +187,51 @@ LIMIT ..., ...
 # FROM ..., ... -> ON -> (LEFT / RIGHT JOIN) -> WHERE -> GROUP BY -> HAVING -> SELECT -> DISTINCT ->
 # ORDER BY -> LIMIT
 
+# 课后练习
 
+# 1.where子句可否使用组函数进行过滤?
+# No!
 
+# 2.查询公司员工工资的最大值，最小值，平均值，总和
+SELECT MAX(salary) mas_sal, MIN(salary) min_sal, AVG(salary) avg_sal, SUM(salary) sum_sal
+FROM employees;
 
+# 3.查询各job_id的员工工资的最大值，最小值，平均值，总和
+SELECT job_id, MAX(salary), MIN(salary), AVG(salary), SUM(salary)
+FROM employees
+GROUP BY job_id;
 
+# 4.选择具有各个job_id的员工人数
+SELECT job_id, COUNT(*)
+FROM employees
+GROUP BY job_id;
 
+# 5.查询员工最高工资和最低工资的差距（DIFFERENCE）
+SELECT MAX(salary), MIN(salary), MAX(salary) - MIN(salary) "DIFFERENCE"
+FROM employees;
 
+# 6.查询各个管理者手下员工的最低工资，其中最低工资不能低于6000，没有管理者的员工不计算在内
+SELECT manager_id, MIN(salary)
+FROM employees
+WHERE manager_id IS NOT NULL
+GROUP BY manager_id
+HAVING MIN(salary) >= 6000;
 
+# 7.查询所有部门的名字，location_id，员工数量和平均工资，并按平均工资降序
+SELECT d.department_name, d.location_id, COUNT(e.employee_id), AVG(e.salary)
+FROM departments d
+         LEFT JOIN employees e
+                   ON d.department_id = e.department_id
+GROUP BY d.department_name, d.location_id
+ORDER BY AVG(e.salary) DESC;
 
+SELECT e.employee_id, e.salary, d.department_name, d.location_id
+FROM employees e
+         RIGHT JOIN departments d ON d.department_id = e.department_id;
 
-
+# 8.查询每个工种、每个部门的部门名、工种名和最低工资
+SELECT d.department_name, e.job_id, MIN(e.salary)
+FROM departments d
+         LEFT JOIN employees e
+                   ON e.department_id = d.department_id
+GROUP BY d.department_name, e.job_id;
