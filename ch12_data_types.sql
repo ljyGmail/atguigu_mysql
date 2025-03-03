d506dc59cd6d164280b8730036b36b696346a90b
ljyGmail <liangjy881@gmail.com>
2025-03-03 16:26:40 +0900

    61 浮点数、定点数与位类型讲解

--- ch12_data_types.sql
+++ ch12_data_types.sql
@@ -89,3 +89,131 @@ VALUES (2412321);
 INSERT INTO test_int3
 VALUES (4294967296);
 
+
+# 3. 浮点类型
+CREATE TABLE test_double1
+(
+    f1 FLOAT,
+    f2 FLOAT(5, 2),
+    f3 DOUBLE,
+    f4 DOUBLE(5, 2)
+);
+
+DESC test_double1;
+
+INSERT INTO test_double1(f1, f2)
+VALUES (123.45, 123.45);
+
+SELECT *
+FROM test_double1;
+
+INSERT INTO test_double1(f3, f4)
+VALUES (123.45, 123.456);
+# 存在四舍五入
+
+# Out of range value for column 'f4' at row 1
+INSERT INTO test_double1(f3, f4)
+VALUES (123.45, 1234.456);
+
+# Out of range value for column 'f4' at row 1
+INSERT INTO test_double1(f3, f4)
+VALUES (123.45, 999.995);
+
+# 测试FLOAT和DOUBLE的精度问题
+CREATE TABLE test_double2
+(
+    f1 DOUBLE
+);
+
+INSERT INTO test_double2
+VALUES (0.47),
+       (0.44),
+       (0.19);
+
+SELECT *
+FROM test_double2;
+
+SELECT SUM(f1)
+FROM test_double2;
+
+SELECT SUM(f1) = 1.1, 1.1 = 1.1
+FROM test_double2;
+
+# 4. 定点数类型
+CREATE TABLE test_decimal1
+(
+    f1 DECIMAL,
+    f2 DECIMAL(5, 2)
+);
+
+DESC test_decimal1;
+
+INSERT INTO test_decimal1(f1)
+VALUES (123),
+       (123.45);
+
+SELECT *
+FROM test_decimal1;
+
+INSERT INTO test_decimal1(f2)
+VALUES (999.99);
+
+INSERT INTO test_decimal1(f2)
+VALUES (67.567);
+# 存在四舍五入
+
+# Out of range value for column 'f2' at row 1
+INSERT INTO test_decimal1(f2)
+VALUES (1267.567);
+
+# Out of range value for column 'f2' at row 1
+INSERT INTO test_decimal1(f2)
+VALUES (999.995);
+
+# 演示DECIMAL替换DOUBLE。体现精度。
+ALTER TABLE test_double2
+    MODIFY f1 decimal(5, 2);
+
+DESC test_double2;
+
+SELECT SUM(f1)
+FROM test_double2;
+
+SELECT SUM(f1) = 1.1, 1.1 = 1.1
+FROM test_double2;
+
+# 5. 位类型: BIT
+CREATE TABLE test_bit1
+(
+    f1 BIT,
+    f2 BIT(5),
+    f3 BIT(64)
+);
+
+DESC test_bit1;
+
+INSERT INTO test_bit1(f1)
+VALUES (0),
+       (1);
+
+SELECT *
+FROM test_bit1;
+
+# Out of range value for column 'f1' at row 1
+INSERT INTO test_bit1(f1)
+VALUES (2);
+
+INSERT INTO test_bit1(f2)
+VALUES (31);
+
+# Out of range value for column 'f2' at row 1
+INSERT INTO test_bit1(f2)
+VALUES (32);
+
+SELECT BIN(f1), BIN(f2), HEX(f1), HEX(f2)
+FROM test_bit1;
+
+# 此时+0以后，可以以十进制的方式显示数据
+SELECT f1 + 0, f2 + 0
+FROM test_bit1;
+
