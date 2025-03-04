1b1e6029a546c5af2e620eca0df71866e89b245e
ljyGmail <liangjy881@gmail.com>
2025-03-04 10:15:19 +0900

    62 日期时间类型讲解

--- ch12_data_types.sql
+++ ch12_data_types.sql
@@ -217,3 +217,179 @@ FROM test_bit1;
 SELECT f1 + 0, f2 + 0
 FROM test_bit1;
 
+# 6. 日期类型
+# 6.1 YEAR类型
+CREATE TABLE test_year
+(
+    f1 YEAR,
+    f2 YEAR(4)
+);
+
+DESC test_year;
+
+INSERT INTO test_year(f1)
+VALUES ('2021'),
+       (2022);
+
+SELECT *
+FROM test_year;
+
+INSERT INTO test_year(f1)
+VALUES ('2155');
+
+# Out of range value for column 'f1' at row 1
+INSERT INTO test_year(f1)
+VALUES ('2156');
+
+INSERT INTO test_year(f1)
+VALUES ('69'),
+       ('70');
+
+INSERT INTO test_year(f1)
+VALUES (0),
+       ('00');
+
+# 6.2 DATE类型
+CREATE TABLE test_date1
+(
+    f1 DATE
+);
+
+DESC test_date1;
+
+INSERT INTO test_date1
+VALUES ('2020-10-01'),
+       ('20201001'),
+       (20201001);
+
+INSERT INTO test_date1
+VALUES ('00-01-01'),
+       ('000101'),
+       ('69-10-01'),
+       ('691001'),
+       ('70-01-01'),
+       ('700101'),
+       ('99-01-01'),
+       ('990101');
+
+INSERT INTO test_date1
+VALUES (000301),
+       (690301),
+       (700301),
+       (990301); # 存在隐式转换
+
+INSERT INTO test_date1
+VALUES (CURDATE()),
+       (CURRENT_DATE()),
+       (NOW());
+
+SELECT *
+FROM test_date1;
+
+# 6.3 TIME类型
+CREATE TABLE test_time1
+(
+    f1 TIME
+);
+
+DESC test_time1;
+
+INSERT INTO test_time1
+VALUES ('2 12:30:29'),
+       ('12:35:29'),
+       ('12:40'),
+       ('2 12:40'),
+       ('1 05'),
+       ('45');
+
+INSERT INTO test_time1
+VALUES ('123520'),
+       (124011),
+       (1210);
+
+INSERT INTO test_time1
+VALUES (NOW()),
+       (CURRENT_TIME()),
+       (CURTIME());
+
+SELECT *
+FROM test_time1;
+
+# 6.4 DATETIME类型
+CREATE TABLE test_datetime1
+(
+    dt DATETIME
+);
+
+DESC test_datetime1;
+
+INSERT INTO test_datetime1
+VALUES ('2021-01-01 06:50:30'),
+       ('20210101065030');
+
+INSERT INTO test_datetime1
+VALUES ('99-01-01 00:00:00'),
+       ('990101000000'),
+       ('20-01-01 00:00:00'),
+       ('200101000000');
+
+INSERT INTO test_datetime1
+VALUES (20200101000000),
+       (200101000000),
+       (19990101000000),
+       (990101000000);
+
+INSERT INTO test_datetime1
+VALUES (CURRENT_TIMESTAMP()),
+       (NOW()),
+       (SYSDATE());
+
+SELECT *
+FROM test_datetime1;
+
+# 6.5 TIMESTAMP类型
+CREATE TABLE test_timestamp1
+(
+    ts TIMESTAMP
+);
+
+INSERT INTO test_timestamp1
+VALUES ('1999-01-01 03:04:50'),
+       ('19990101030405'),
+       ('99-01-01 03:04:05'),
+       ('990101030405');
+
+INSERT INTO test_timestamp1
+VALUES ('2020@01@01@00@00@00'),
+       ('20@01@01@00@00@00');
+
+INSERT INTO test_timestamp1
+VALUES (CURRENT_TIMESTAMP()),
+       (NOW());
+
+# Incorrect datetime value
+INSERT INTO test_timestamp1
+VALUES ('2038-01-20 03:14:07');
+
+SELECT *
+FROM test_timestamp1;
+
+# 对比 DATETIME 和 TIMESTAMP
+CREATE TABLE temp_time
+(
+    d1 DATETIME,
+    d2 TIMESTAMP
+);
+
+INSERT INTO temp_time
+VALUES ('2021-9-2 14:45:52', '2021-9-2 14:45:52');
+
+INSERT INTO temp_time
+VALUES (NOW(), NOW());
+
+# 修改当前的时区
+SET TIME_ZONE = '+8:00';
+
+SELECT *
+FROM temp_time;
+
