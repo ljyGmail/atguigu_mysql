a3acb244304a1298c50ff7e3dd620ad8cd5fff5b
ljyGmail <liangjy881@gmail.com>
2025-03-04 16:59:21 +0900

    67 非空约束的使用

--- ch13_constraints.sql
+++ ch13_constraints.sql
@@ -31,3 +31,61 @@ CREATE TABLE时添加约束
 ALTER TABLE时增加约束、删除约束
  */
 
+# 2. 如何查看表中的约束
+SELECT *
+FROM information_schema.TABLE_CONSTRAINTS
+WHERE TABLE_NAME = 'employees';
+
+CREATE DATABASE dbtest13;
+
+USE dbtest13;
+
+# 3. NOT NULL(非空约束)
+# 3.1 在CREATE TABLE时添加约束
+CREATE TABLE test1
+(
+    id        INT         NOT NULL,
+    last_name VARCHAR(15) NOT NULL,
+    email     VARCHAR(25),
+    salary    DECIMAL(10, 2)
+);
+
+DESC test1;
+
+INSERT INTO test1(id, last_name, email, salary)
+VALUES (1, 'Tom', 'tom@126.com', 3400);
+
+# 错误: Column 'last_name' cannot be null
+INSERT INTO test1(id, last_name, email, salary)
+VALUES (2, NULL, 'tom@126.com', 3400);
+
+# 错误: Column 'id' cannot be null
+INSERT INTO test1(id, last_name, email, salary)
+VALUES (NULL, 'Jerry', 'jerry@126.com', 3400);
+
+# 错误: Field 'last_name‘ doesn't have a default vaule
+INSERT INTO test1(id, email)
+VALUES (2, 'abc@126.com');
+
+# 错误: Column 'last_name' cannot be null
+UPDATE test1
+SET email=NULL
+WHERE id = 1;
+
+SELECT *
+FROM test1;
+
+UPDATE test1
+SET email='tom@126.com'
+WHERE id = 1;
+
+# 3.2 在ALTER TABLE时添加约束
+DESC test1;
+
+ALTER TABLE test1
+    MODIFY email VARCHAR(25) NOT NULL;
+
+# 3.3 在ALTER TABLE时删除约束
+ALTER TABLE test1
+    MODIFY email VARCHAR(25) NULL;
+
