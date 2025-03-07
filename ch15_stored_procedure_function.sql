26bf920c5f25297d0e38cc870d28dedc1980cf67
ljyGmail <liangjy881@gmail.com>
2025-03-07 10:01:38 +0900

    81 存储过程与函数的查看修改和删除

--- ch15_stored_procedure_function.sql
+++ ch15_stored_procedure_function.sql
@@ -221,3 +221,39 @@ DELIMITER ;
 SET @dept_id := 50;
 SELECT count_by_id(@dept_id);
 
+
+# 3. 存储过程、存储函数的查看
+
+# 方式① 使用SHOW CREATE语句查看存储过程和函数的创建信息
+SHOW CREATE PROCEDURE show_mgr_name;
+
+SHOW CREATE FUNCTION count_by_id;
+
+# 方式② 使用SHOW STATUS语句查看存储过程和函数的状态信息
+SHOW PROCEDURE STATUS;
+
+SHOW PROCEDURE STATUS LIKE 'show_max_salary';
+
+SHOW FUNCTION STATUS LIKE 'email_by_id';
+
+# 方式③ 从information_schema.Routines表中查看存储过程和函数的信息
+SELECT *
+FROM information_schema.ROUTINES
+WHERE ROUTINE_NAME = 'email_by_id'
+  AND ROUTINE_TYPE = 'FUNCTION';
+
+SELECT *
+FROM information_schema.ROUTINES
+WHERE ROUTINE_NAME = 'show_min_salary'
+  AND ROUTINE_TYPE = 'PROCEDURE';
+
+# 4. 存储过程、函数的修改
+ALTER PROCEDURE show_max_salary
+    SQL SECURITY INVOKER
+    COMMENT '查询最高工资';
+
+# 5. 存储过程、函数的删除
+DROP FUNCTION IF EXISTS count_by_id;
+
+DROP PROCEDURE IF EXISTS show_min_salary;
+
